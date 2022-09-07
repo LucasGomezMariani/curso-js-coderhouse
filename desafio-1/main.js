aa373da5dfb9e611c6813bc3fad8b180fb33d844
unknown <lucasgomezmariani777@gmail.com>
2022-09-07 20:17:03 -0300

    7/9 - meti la verificacion de datos dentro de una funcion, para reciclar y achicar el codigo

--- desafio-1/main.js
+++ desafio-1/main.js
@@ -1,3 +1,5 @@
+// Esta es la primer version del algoritmo.
+
 /* let nombreAlumno = prompt('Ingrese el nombre del alumno/a:')
 
 function promedio(calificacion1, calificacion2, calificacion3) {
@@ -18,6 +20,73 @@ function promedio(calificacion1, calificacion2, calificacion3) {
 promedio(); */
 
 
+
+
+
+
+// Esta es la segunda version
+
+/* function datosAlumno() {
+
+
+    let nombreAlumno = prompt('Ingrese el nombre del alumno/a:');
+
+    let calificacion1 = parseInt(prompt('Ingrese la primer calificación: '))
+
+    let calificacion2 = parseInt(prompt('Ingrese la segunda calificación: '))
+
+    let calificacion3 = parseInt(prompt('Ingrese la tercer calificación: '))
+
+
+    promedio(nombreAlumno,calificacion1,calificacion2,calificacion3)
+
+    
+
+}
+
+
+datosAlumno();  // FIJATE QUE SOLO LLAMO A ESTA FUNCION que a su ves llama a otras
+
+
+function promedio(nombreAlumno,calificacion1, calificacion2, calificacion3) {
+
+    
+
+    let promedio = (calificacion1+calificacion2+calificacion3) / 3;
+
+
+    mostrarData(nombreAlumno,promedio)
+
+ 
+
+}
+
+
+function mostrarData(nombreAlumno,promedio){
+
+
+    if (promedio>=6) {
+
+        alert('Felicitaciones '+nombreAlumno+ ' tu calificación es '+promedio+' y pasas a estar aprobado/a!')
+
+        
+
+    } else {
+
+        alert('Tenemos malas noticias '+nombreAlumno+ '\n tu calificación es '+ promedio+ ' y no alcanza para que apruebes.')
+
+        
+
+    }
+
+}
+ */
+
+
+
+
+// Esta es la tercera version
+/* 
 function datosAlumno() {
 
 
@@ -113,6 +182,77 @@ function mostrarData(nombreAlumno,promedio){
         alert('Tenemos malas noticias '+nombreAlumno+ '\n tu calificación es '+ promedio+ ' y no alcanza para que apruebes.')
 
         
+    }
+
+} */
+
+
+
+
+// Esta es la cuarta version.
+
+function datosAlumno() {
+
+    let nombreAlumno;
+    let nombreCorrecto = true
+    let calificacion1;
+    let calificacion2;
+    let calificacion3;
+
+    do {
+        nombreAlumno = prompt('Ingrese el nombre del alumno/a:');
+        if (nombreAlumno == '') {
+            alert('El nombre ingresado es invalido')
+        } else {
+            nombreCorrecto=false
+        }
+    } while (nombreCorrecto);
+
+
+    promedio(nombreAlumno,calificacion(calificacion1),calificacion(calificacion2),calificacion(calificacion3))
+    
+}
+
+function calificacion (calificacion) {
+
+    let verificacion = true
+
+    do {
+        calificacion = Number(prompt('Ingrese la nota de la calificación. Debe ser un numero del 1 - 10: '))
+
+        if (calificacion == '' || calificacion<1 || calificacion>10) {
+            alert('El numero ingresado es invalido. Pruebe con un numero del 1 - 10')
+        } else {
+            verificacion = false
+            return calificacion
+        }
+
+    } while (verificacion);
+
+}
+
+datosAlumno();  
+
+
+function promedio(nombreAlumno,calificacion1, calificacion2, calificacion3) {
+
+    let promedio = (calificacion1+calificacion2+calificacion3) / 3;
+
+    mostrarData(nombreAlumno,promedio)
+
+}
+
+
+function mostrarData(nombreAlumno,promedio){
+
+    if (promedio>=6) {
+
+        alert('Felicitaciones '+nombreAlumno+ ' tu calificación es '+promedio+' y pasas a estar aprobado/a!')
+
+    } else {
+
+        alert('Tenemos malas noticias '+nombreAlumno+ '\n tu calificación es '+ promedio+ ' y no alcanza para que apruebes.')
+      
     }
 
 }
