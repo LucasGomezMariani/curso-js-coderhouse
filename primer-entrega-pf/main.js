b64868c78d00285417d9fc6e0c30d3dc2f3c69c4
Lucas Gomez <lucasgomezmariani777@gmail.com>
2022-09-14 20:44:13 -0300

    14/09 - Agregado la Primer entrega del proyecto final. En proceso.

---
+++ primer-entrega-pf/main.js
@@ -0,0 +1,101 @@
+// Array de usuarios
+const UsersList = []
+
+
+// Molde de usuarios
+class UserCreate {
+    constructor(nombre, apellido, email, edad) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.email = email
+        this.edad = edad
+    }
+    presentarme() {
+        alert('Hola mi nombre es ' + this.nombre + ' ' + this.apellido + ' y tengo ' + this.edad + ' años de edad.');
+    }
+    
+}
+// Function creadora de usuarios.
+function CrearUsuario() {
+    do {
+        // Algoritmo que crea al usuario
+        const usuario = new UserCreate( getName(), getSurname(), getEmail(), getAge() );
+        // console.log(usuario1);
+        usuario.presentarme();
+        UsersList.push(usuario);
+
+        // Seguir cargando usuarios?
+        validar = confirm('Quiere crear otro usuario?');
+
+    } while (validar); 
+}
+
+CrearUsuario()
+
+
+console.log(UsersList);
+
+
+
+
+function getName(nombre) {
+
+    let acept = true;
+
+    while (acept) {
+        nombre = prompt('Ingrese su nombre')
+
+        if (!nombre) {
+            alert('El nombre ingresado no es correcto')
+        } else {
+            acept = false
+            return nombre
+        }
+    }
+}
+function getSurname(apellido) {
+
+    let acept = true;
+
+    while (acept) {
+        apellido = prompt('Ingrese su apellido')
+
+        if (!apellido) {
+            alert('El apellido ingresado no es correcto')
+        } else {
+            acept = false
+            return apellido
+        }
+    }
+}
+function getEmail(email) {
+
+    let acept = true;
+
+    while (acept) {
+        email = prompt('Ingrese su email')
+
+        if (!email) {
+            alert('El email ingresado no es correcto')
+        } else {
+            acept = false
+            return email
+        }
+    }
+}
+function getAge(edad) {
+
+    let acept = true;
+
+    while (acept) {
+
+        edad = Number(prompt('Ingrese su edad. Recuerde que debe ser mayor de edad.'))
+
+        if ( edad < 18 || !edad) {
+            alert('La edad ingresada no es correcta')
+        } else {
+            acept = false
+            return edad
+        }
+    }
+}
