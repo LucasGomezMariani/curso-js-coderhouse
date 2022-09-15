81f5f0cf91351b542b92284b8834f18945e7f8da
Lucas Gomez <lucasgomezmariani777@gmail.com>
2022-09-15 09:24:55 -0300

    15/09 - Agregado el algoritmo para loguearse en la pagina

--- primer-entrega-pf/main.js
+++ primer-entrega-pf/main.js
@@ -1,25 +1,30 @@
-// Array de usuarios
-const UsersList = []
+// 1- Registrarse
 
+
+// Array de usuarios
+let UsersList = []
 
 // Molde de usuarios
 class User {
-    constructor(nombre, apellido, email, edad) {
+    constructor(nombre, apellido, email, edad, password) {
         this.nombre = nombre
         this.apellido = apellido
         this.email = email
         this.edad = edad
+        this.password = password
     }
     presentarme() {
         alert('Hola mi nombre es ' + this.nombre + ' ' + this.apellido + ' y tengo ' + this.edad + ' años de edad.');
     }
-    
+
 }
+
 // Function creadora de usuarios.
 function CrearUsuario() {
     do {
         // Algoritmo que crea al usuario
-        const usuario = new User ( getName(), getSurname(), getEmail(), getAge() );
+        const usuario = new User(getName(), getSurname(), getEmail(), getAge(), getPassword());
+
         // console.log(usuario1);
         usuario.presentarme();
         UsersList.push(usuario);
@@ -27,13 +32,13 @@ function CrearUsuario() {
         // Seguir cargando usuarios?
         validar = confirm('Quiere crear otro usuario?');
 
-    } while (validar); 
+    } while (validar);
 }
 
 CrearUsuario()
 console.log(UsersList);
 
-
+// Verificacion de datos.
 function getName(nombre) {
 
     let acept = true;
@@ -87,11 +92,99 @@ function getAge(edad) {
 
         edad = Number(prompt('Ingrese su edad. Recuerde que debe ser mayor de edad.'))
 
-        if ( edad < 18 || !edad) {
+        if (edad < 18 || !edad) {
             alert('La edad ingresada no es correcta')
         } else {
             acept = false
             return edad
+        }
+    }
+}
+function getPassword(password) {
+
+    let acept = true;
+
+    while (acept) {
+
+        password = prompt('Ingrese una contraseña. Debe ser de minimo de 8 caracteres.')
+
+        if (password.length < 8) {
+            alert('La contraseña ingresada es demasiado corta')
+        } else {
+            acept = false
+            return password
+        }
+    }
+}
+
+
+
+
+
+
+// 2- Iniciar seción
+login();
+
+
+
+
+
+// Funcion para iniciar seción.
+
+function login() {
+    let login = confirm('Desea iniciar sesión?')
+    while (login) {
+        confirmUser();
+
+        login = false
+    }
+}
+
+// comparacion y validacion del nombre de usuario (propiedad nombre)
+
+function confirmUser() {
+
+    let validar = true;
+
+    while (validar) {
+
+        let loginUser = prompt('Ingrese su nombre')
+        const usuarioEncontrado = UsersList.map((item) => item.nombre === loginUser)
+
+        // console.log(usuarioEncontrado);
+
+        if (!usuarioEncontrado) {
+            alert('El usuario no existe, por favor vuelva a intentar.')
+
+        } else {
+            console.log('Nombre ingresado con exito.');
+            confirmPassword();
+
+            validar = false
+        }
+    }
+}
+
+// comparacion y validacion del password (propiedad password)
+
+function confirmPassword() {
+
+    let validar = true;
+
+    while (validar) {
+
+        let loginPassword = prompt('Ingrese su contraseña')
+        const correctPassword = UsersList.map((item) => item.password === loginPassword)
+
+        // console.log(correctPassword);
+
+        if (!correctPassword) {
+            alert('La contraseña es incorrecta. Por favor vuelva a intentar')
+
+        } else {
+            console.log('Contraseña ingresada con exito.');
+            alert('Datos ingresados correctamente. Bienvenido!')
+            validar = false
         }
     }
 }
