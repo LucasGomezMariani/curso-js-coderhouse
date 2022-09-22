00f2971451a4bc67db558fdc9db9ed1d82c309b8
Lucas Gomez <lucasgomezmariani777@gmail.com>
2022-09-22 08:54:38 -0300

    22/09 - corregidas las malas practicas dentro del HTML para invocar eventos. Cambiada la forma de llamar los datos del formulario para la creacion del usuario

--- proyecto/registo-login.js
+++ proyecto/registo-login.js
@@ -1,8 +1,5 @@
-// 1- Registrarse
-
-
 // Array de usuarios
-let UsersList = []
+let UsersList = [];
 
 // Molde de usuarios
 class User {
@@ -13,113 +10,156 @@ class User {
         this.edad = edad
         this.password = password
     }
-    presentarme() {
-        alert('Hola mi nombre es ' + this.nombre + ' ' + this.apellido + ' y tengo ' + this.edad + ' años de edad.');
+    saludar() {
+        alert(`Bienvenidx ${this.nombre}, ahora podras disfrutar de la experiencia completa nuestra web :D`);
     }
-
 }
 
-// Function creadora de usuarios.
-function CrearUsuario() {
+//REGISTRO
 
-    // Algoritmo que crea al usuario
-    const usuario = new User(
-        getName(),
-        getSurname(),
-        getEmail(),
-        getAge(),
-        getPassword()
-    );
+// 1- capturamos el formulario
+const form = document.getElementById('registerForm');
 
-    // console.log(usuario1);
-    usuario.presentarme();
+// 2- Agregamos un event
+form.addEventListener('submit', cargarDatos);
+
+// 3- Funcion para cargar datos.
+function cargarDatos(e) {
+    // metodo para no recargar la pagina
+    e.preventDefault();
+
+    // capturamos los datos del formulario
+    const formulario = new FormData(registerForm);
+    const nombre = formulario.get('first_name');
+    const apellido = formulario.get('last_name');
+    const email = formulario.get('email');
+    const edad = formulario.get('age');
+    const password = formulario.get('password');
+
+    // cargamos los datos capturados, dentro del molde de usuarios
+    const usuario = new User( nombre, apellido, email, edad, password );
+
+    // pusheamos el usuario dentro del array
     UsersList.push(usuario);
+
     console.log(UsersList);
-
-    let form = document.getElementById('register_form');
-    form.classList.add('d-none')
-
+    
+    // capturamos la seccion y la ocultamos con display: none
+    let containerForm = document.getElementById('registerContainer');
+    containerForm.classList.add('d-none')
+    
+    usuario.saludar();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Verificacion de datos.
-function getName(nombre) {
+// function getName(nombre) {
 
-    let acept = true;
+//     let acept = true;
 
-    while (acept) {
-        nombre = document.getElementById('first_name').value;
+//     while (acept) {
+//         nombre = document.getElementById('first_name').value;
 
-        if (!nombre) {
-            // alert('El nombre ingresado no es correcto')
-        } else {
-            acept = false
-            return nombre
-        }
-    }
-}
-function getSurname(apellido) {
+//         if (!nombre) {
+//             // alert('El nombre ingresado no es correcto')
+//         } else {
+//             acept = false
+//             return nombre
+//         }
+//     }
+// }
+// function getSurname(apellido) {
 
-    let acept = true;
+//     let acept = true;
 
-    while (acept) {
-        apellido = document.getElementById('last_name').value;
+//     while (acept) {
+//         apellido = document.getElementById('last_name').value;
 
-        if (!apellido) {
-            // alert('El apellido ingresado no es correcto')
-        } else {
-            acept = false
-            return apellido
-        }
-    }
-}
-function getEmail(email) {
+//         if (!apellido) {
+//             // alert('El apellido ingresado no es correcto')
+//         } else {
+//             acept = false
+//             return apellido
+//         }
+//     }
+// }
+// function getEmail(email) {
 
-    let acept = true;
+//     let acept = true;
 
-    while (acept) {
-        email = document.getElementById('email').value;
+//     while (acept) {
+//         email = document.getElementById('email').value;
 
-        if (!email) {
-            // alert('El email ingresado no es correcto')
-        } else {
-            acept = false
-            return email
-        }
-    }
-}
-function getAge(edad) {
+//         if (!email) {
+//             // alert('El email ingresado no es correcto')
+//         } else {
+//             acept = false
+//             return email
+//         }
+//     }
+// }
+// function getAge(edad) {
 
-    let acept = true;
+//     let acept = true;
 
-    while (acept) {
+//     while (acept) {
 
-        edad = document.getElementById('age').value;
+//         edad = document.getElementById('age').value;
 
-        if (edad < 18 || !edad) {
-            // alert('La edad ingresada no es correcta')
-        } else {
-            acept = false
-            return edad
-        }
-    }
-}
-function getPassword(password) {
+//         if (edad < 18 || !edad) {
+//             // alert('La edad ingresada no es correcta')
+//         } else {
+//             acept = false
+//             return edad
+//         }
+//     }
+// }
+// function getPassword(password) {
 
-    let acept = true;
+//     let acept = true;
 
-    while (acept) {
+//     while (acept) {
 
-        password = document.getElementById('password').value;
+//         password = document.getElementById('password').value;
 
-        if (password.length < 8) {
-            // alert('La contraseña ingresada es demasiado corta')
-        } else {
-            acept = false
-            return password
-        }
-    }
-}
+//         if (password.length < 8) {
+//             // alert('La contraseña ingresada es demasiado corta')
+//         } else {
+//             acept = false
+//             return password
+//         }
+//     }
+// }
 
 
 
